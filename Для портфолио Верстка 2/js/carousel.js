47e2632d48d41f20f0b697bccd68151b576a9efa
---- <j.wind@internrt.ru>
2024-06-23 20:15:22 +0300

    first commit

---
+++ Для портфолио Верстка 2/js/carousel.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 4,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
+});
